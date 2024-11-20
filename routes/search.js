4514d0404876cb5ed7ae5bbc86c1099b4ba59ec2
Monica Hellberg <monicahellberg11@gmail.com>
2024-11-20 20:59:36 +0100

    Added search functionality

--- routes/search.js
+++ routes/search.js
@@ -6,6 +6,24 @@ const Database = require('better-sqlite3');
 const dbPath = path.resolve(__dirname, '../db/freakyfashion.db');
 const db = new Database(dbPath);
 
+router.get('/', (req, res) => {
+  const query = req.query.q;
+  let products = [];
 
+  if (query) {
+    products = db.prepare(`
+      SELECT * 
+      FROM products 
+      WHERE name LIKE ?
+    `).all(`%${query}%`);
+  }
+
+  res.render('search', {
+    title: 'Sökresultat',
+    query: query,
+    products: products,
+    productCount: products.length
+  });
+});
 
 module.exports = router;
